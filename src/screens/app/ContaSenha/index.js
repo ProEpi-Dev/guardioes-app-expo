02c0f7091472fc6266cdca08649457eb2c5a84df
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2025-10-30 10:56:19 -0300

    Adicionando a tela de alteracao de senha

--- src/screens/app/ContaSenha/index.js
+++ src/screens/app/ContaSenha/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 
 import { styles } from './styles';
 
 export function ContaSenha() {
+  const [oldPassword, setOldPassword] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   return (
     <View style={styles.container}>
-        <Text>Tela de alteração de senha</Text>
+      <View style={styles.form}>
+        <Text style={styles.label}>Senha Atual</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setOldPassword}
+            value={oldPassword}
+            keyboardType='password'
+          />
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Nova Senha</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            keyboardType='password'
+          />
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Confirmar Nova Senha</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setConfirmPassword}
+            value={confirmPassword}
+            keyboardType='password'
+          />
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
