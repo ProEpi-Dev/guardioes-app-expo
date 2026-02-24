ecc6c491c153faa8a503b48b6012070fbbcedc1a
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2026-02-24 10:34:26 -0300

    Fix: Adjusting footer size in ArticleScreen to render de content inside the box corretly

--- src/screens/app/ArticleScreen/index.tsx
+++ src/screens/app/ArticleScreen/index.tsx
@@ -4,19 +4,24 @@ import { RootStackParamList } from '../../../types/article';
 import { ArticleWebView } from '../../../components/ArticleWebView';
 import { CustomHeader } from '../../../components/CustomHeader';
 import { useAuth } from '../../../contexts/AuthContext';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Article'>;
 
 export default function ArticleScreen({ route }: Props) {
   const { user } = useAuth();
   const { article } = route.params;
+  const insets = useSafeAreaInsets();
 
   return (
     <>
       <CustomHeader userName={user?.name} showBackButton={true}/>
-      <ArticleWebView
-        content={article.content}
-      />
+      <View style={{ flex: 1, paddingBottom: insets.bottom + 90 }}>
+        <ArticleWebView
+          content={article.content}
+        />
+      </View>
     </>
   );
 }
